91ac359644facf38c991345662d156a9663701ae
Koda-Pig <josh.koter@gmail.com>
2021-12-24 11:48:15 +0200

    ...
    ...

--- next.config.js
+++ next.config.js
@@ -1,26 +1,9 @@
-const withImages = require("next-images");
-const withCSS = require('next-css')
-const withVideos = require('next-videos')
-
-
-module.exports = withCSS(withImages(withVideos( {
-  reactStrictMode: true,
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
+module.exports = {
+  future: {
+    webpack5: true,
   },
-  webpack: (config, { isServer }) => {
-      if (!isServer) {
-          // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-          config.resolve.fallback = {
-              fs: false
-          }
-      }
-
-      return config;
-  }
-})))
+  webpack: function (config, options) {
+    config.experiments = {};
+    return config;
+  },
+};
