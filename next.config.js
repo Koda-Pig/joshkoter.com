1c965bdd2da71776c26120684a5504e22d2b58c4
Koda-Pig <josh.koter@gmail.com>
2021-12-24 12:16:30 +0200

    ...
    ...

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  future: {
-    webpack5: true,
-  },
-  webpack: function (config, options) {
-    config.experiments = {};
-    return config;
-  },
-};
+  reactStrictMode: true,
+}
+
+const withVideos = require('next-videos')
+
+module.exports = withVideos()
