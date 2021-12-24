ad6e9ee4506880c05e5e770921e99c9a39eeb7b4
Koda-Pig <josh.koter@gmail.com>
2021-12-24 13:00:48 +0200

    ...
    ...

--- next.config.js
+++ next.config.js
@@ -1,35 +1,7 @@
-module.exports = [{
-  entry: './src/style/style.scss',
-  output: {
-     // This is necessary for webpack to compile
-     // But we never use style-bundle.js
-     filename: 'style-bundle.js'
-  },
-  module: {
-     rules: [{
-        test: /\.css$/,
-        use: [{
-              loader: 'file-loader',
-              options: {
-                 name: 'bundle.css'
-              }
-           },
-           'extract-loader',
-           'css-loader',
-        ]
-     }]
-  }
-},
-{
-  entry: './src/app.js',
-  output: {
-     filename: 'bundle.js'
-  },
-  module: {
-     rules: [{
-        test: /\.js$/,
-        use: 'babel-loader'
-     }]
-  }
-}
-];
+module.exports = {
+   reactStrictMode: true,
+ }
+ 
+ const withVideos = require('next-videos')
+ 
+ module.exports = withVideos()
