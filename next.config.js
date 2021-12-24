933f6fcdd49e8f58143d980bde82636f23df4f2b
Koda-Pig <josh.koter@gmail.com>
2021-12-24 10:54:59 +0200

    Updates to next.config.js
    ...

--- next.config.js
+++ next.config.js
@@ -1,7 +1,31 @@
 module.exports = {
-  reactStrictMode: false,
+  reactStrictMode: true,
 }
 
 const withVideos = require('next-videos')
 
 module.exports = withVideos()
+
+const withCSS = require('@zeit/next-css')
+const withLess = require('@zeit/next-less')
+const withSass = require('@zeit/next-sass')
+
+
+module.exports = withCSS(
+  withLess(
+    withSass({
+      lessLoaderOptions: {
+        javascriptEnabled: true,
+      },
+      webpack: config => {
+        config.module.rules.push(
+          {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader'],
+          }
+        );
+        return config;
+      }
+    })
+  )
+)
