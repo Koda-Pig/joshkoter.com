c151f8db964dd9395b4c8d8bbc4cf51470d440a2
Koda-Pig <josh.koter@gmail.com>
2021-12-24 12:37:01 +0200

    ...
    ...

--- next.config.js
+++ next.config.js
@@ -1,14 +1,35 @@
-module.exports = {
+module.exports = [{
+  entry: './src/style/style.scss',
+  output: {
+     // This is necessary for webpack to compile
+     // But we never use style-bundle.js
+     filename: 'style-bundle.js'
+  },
   module: {
-    loaders: [{
-          test: /\.css$/,
-          loader: 'style!css'
-       },
-       {
-          test: /\.jsx?$/,
-          loader: 'babel?stage=0',
-          exclude: /node_modules/
-       },
-    ]
- }
+     rules: [{
+        test: /\.css$/,
+        use: [{
+              loader: 'file-loader',
+              options: {
+                 name: 'bundle.css'
+              }
+           },
+           'extract-loader',
+           'css-loader',
+        ]
+     }]
+  }
+},
+{
+  entry: './src/app.js',
+  output: {
+     filename: 'bundle.js'
+  },
+  module: {
+     rules: [{
+        test: /\.js$/,
+        use: 'babel-loader'
+     }]
+  }
 }
+];
