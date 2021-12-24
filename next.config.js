6d1a80e7cc834eea64a22e9555268e0f278eba8a
Koda-Pig <josh.koter@gmail.com>
2021-12-24 10:59:43 +0200

    Added css-loader
    ...

--- next.config.js
+++ next.config.js
@@ -5,27 +5,3 @@ module.exports = {
 const withVideos = require('next-videos')
 
 module.exports = withVideos()
-
-const withCSS = require('@zeit/next-css')
-const withLess = require('@zeit/next-less')
-const withSass = require('@zeit/next-sass')
-
-
-module.exports = withCSS(
-  withLess(
-    withSass({
-      lessLoaderOptions: {
-        javascriptEnabled: true,
-      },
-      webpack: config => {
-        config.module.rules.push(
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          }
-        );
-        return config;
-      }
-    })
-  )
-)
