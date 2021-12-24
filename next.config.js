32447a893073e4da2586989cf617e8ce0ff02967
Koda-Pig <josh.koter@gmail.com>
2021-12-24 11:06:15 +0200

    Added css loader
    ...

--- next.config.js
+++ next.config.js
@@ -5,3 +5,14 @@ module.exports = {
 const withVideos = require('next-videos')
 
 module.exports = withVideos()
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
