55baa1903fa17a4223fe20732cf1e5827e200a8c
Koda-Pig <josh.koter@gmail.com>
2021-12-25 10:35:56 +0200

    ...
    ...

--- next.config.js
+++ next.config.js
@@ -1,13 +1,5 @@
 module.exports = {
    reactStrictMode: true,
-   module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
  }
  
  const withVideos = require('next-videos')
